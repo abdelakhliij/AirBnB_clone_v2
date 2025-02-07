2c2faf9a8255ecd2667cf198ee3bbf49e5823c40
abdelakhliij <riadakhlij@gmail.com>
2025-02-07 13:13:33 -0800

    updated

---
+++ web_flask/my_module.py
@@ -0,0 +1,10 @@
+# my_module.py
+
+"""This is a sample module to demonstrate docstrings."""
+
+class MyClass:
+    """This is a sample class."""
+
+    def my_function(self):
+        """This is a sample function."""
+        pass
