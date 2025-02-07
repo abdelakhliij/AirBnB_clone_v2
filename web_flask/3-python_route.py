4dd4362b943a210cadb247b2d12c19c90b1254b7
abdelakhliij <riadakhlij@gmail.com>
2025-02-07 14:15:52 -0800

    updated

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""starts a Flask web application"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """a function that print hello hbnb! at the root"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """a function that print hbnb! at the root"""
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+    """a function that print C <text> at the root"""
+    text = text.replace("_", " ")
+    return f"C {text}"
+
+
+@app.route('/python/', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python(text="is cool"):
+    """a function that print python <text> at the root and
+    the default value  of the text is 'is cool'"""
+    text = text.replace("_", " ")
+    return f"python {text}"
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
