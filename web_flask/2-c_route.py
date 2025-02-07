8a94775669e965efe9a494918fe9844fc190c6a5
abdelakhliij <riadakhlij@gmail.com>
2025-02-07 13:50:58 -0800

    updated

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""starts a Flask web application"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """a function that print hello hbnb! at the root"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """a function that print hbnb! at the root"""
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def echo(text):
+    """a function that print hbnb! at the root"""
+    return f"C {text}"
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
