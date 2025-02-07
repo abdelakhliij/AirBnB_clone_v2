8553ea0343c5b1a7bbe0505c6818c150cbbea3b9
abdelakhliij <riadakhlij@gmail.com>
2025-02-07 13:37:21 -0800

    updated

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -14,7 +14,7 @@ def hello_hbnb():
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """a function that print hbnb! at the root"""
-    return "HBNB!"
+    return "HBNB"
 
 
 if __name__ == "__main__":
