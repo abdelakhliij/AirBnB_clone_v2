299ca65b2b6981b7aa31556a006752de14a21144
abdelakhliij <riadakhlij@gmail.com>
2025-02-07 12:48:50 -0800

    updated

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 # starts a Flask web application
-#.
-
 from flask import Flask
 
 app = Flask(__name__)
@@ -17,5 +15,5 @@ def hbnb():
     # "returns Hello HBNB!"
     return "HBNB!"
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
